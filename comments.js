91ded7ee7c2bef3bcf426a4aba1ad6dfdeb52070
Tussu08 <imtushar001@gmail.com>
2024-02-07 19:19:02 +0530

    First Commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// console.log(100);
+console.log('hellocworld');
+// console.log('hellocworld');
+console.log(100, 'hellocworld', true);
+const x = 100;
+
+console.log(x)
+
+console.table({ name: 'Tushar', edu: 'ENG' })
+
+
+console.group('Examples')
+console.log(100);
+console.log('hellocworld');
+console.log(100, 'hellocworld', true);
+console.groupEnd
+console.warn('Error')
+console.error('Error')
+const styles = 'padding: 30px; background-color: red;color: white';
+console.log('%cSubhasis Happy Valentine', styles)
+
+
+
+console.log('%cSubhasis Happy Valentine', styles)
---
+++ variable.js
@@ -0,0 +1,6 @@
+// Ways To declare a variable
+// var, let, const
+
+let firstName = 'Tushar';
+let lastName = 'Kumar';
+
